cd891217b8a24d8c9f792f32cb3bcd512f735cbd
abhishek palni <abhishekpalni97@gmail.com>
2026-04-03 15:23:18 +0530

    last commit 999

--- main/main.tf
+++ main/main.tf
@@ -2,8 +2,8 @@ provider "aws" {
     region = "ap-south-1"
   
 }
-resource "aws_iam_role" "welcomeeee" {
-  name = "welcomeeee"
+resource "aws_iam_role" "welcomeeee9" {
+  name = "welcomeeee9"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -22,8 +22,8 @@ resource "aws_iam_role" "welcomeeee" {
   }
 }
 
-resource "aws_iam_role_policy_attachment" "welcomeeee" {
-  role       = aws_iam_role.welcomeeee.name
+resource "aws_iam_role_policy_attachment" "welcomeeee9" {
+  role       = aws_iam_role.welcomeeee9.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
@@ -35,10 +35,10 @@ data "archive_file" "init" {
   output_path = "${path.module}/../new/new.zip"
 }
 
-resource "aws_lambda_function" "examplee" {
+resource "aws_lambda_function" "examplee9" {
   filename      = data.archive_file.init.output_path
-  function_name = "abhi-2026-demooo"
-  role          = aws_iam_role.welcomeeee.arn
+  function_name = "abhi-2026-demooo9"
+  role          = aws_iam_role.welcomeeee9.arn
   handler       = "new.lambda_handler"
   source_code_hash=data.archive_file.init.output_base64sha256
   
